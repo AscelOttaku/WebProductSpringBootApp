de0b7bd1733fe0142f82887de53a919f43adfdd4
Kanybek <zhanybek20065732@gmail.com>
2025-03-10 22:18:42 +0600

    search product function is improved more became easier for reading and understanding this function

--- src/main/java/com/webStore/webStore/service/helper/SearchForProductByDate.java
+++ src/main/java/com/webStore/webStore/service/helper/SearchForProductByDate.java
@@ -9,84 +9,79 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.counting;
+
 public enum SearchForProductByDate {
     MONTH {
         @Override
         public Optional<ProductDTO> findMostPopularProductByDate(int val, List<Product> products) {
-                return products.stream()
-                        .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
-                                product.getSoldAtStoreTime().getMonthValue() == val)
-                        .collect(Collectors.groupingBy(Function.identity(), counting()))
-                        .entrySet().stream()
-                        .max(Map.Entry.<Product, Long>comparingByValue()
-                                .thenComparingDouble(entry -> entry.getKey().getPrice()))
-                        .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+            ToIntFunction<Product> function = product -> product.getSoldAtStoreTime().getMonthValue();
+
+            return getPopularProduct(val, products, function);
         }
 
         @Override
         public Optional<ProductDTO> findMostUnpopularProductByDate(int val, List<Product> products) {
-            return products.stream()
-                    .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
-                            product.getArrivedAtStoreTime().getMonthValue() >= val)
-                    .collect(Collectors.groupingBy(Function.identity(), counting()))
-                    .entrySet().stream()
-                    .min(Map.Entry.<Product, Long>comparingByValue()
-                            .thenComparingDouble(entry -> entry.getKey().getPrice()))
-                    .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+            ToIntFunction<Product> function = product -> product.getArrivedAtStoreTime().getMonthValue();
+
+            return getUnpopularProduct(val, products, function);
         }
     }, YEAR {
         @Override
         public Optional<ProductDTO> findMostPopularProductByDate(int val, List<Product> products) {
-            return products.stream()
-                    .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
-                            product.getSoldAtStoreTime().getYear() == val)
-                    .collect(Collectors.groupingBy(Function.identity(), counting()))
-                    .entrySet().stream()
-                    .max(Map.Entry.<Product, Long>comparingByValue()
-                            .thenComparingDouble(entry -> entry.getKey().getPrice()))
-                    .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+            ToIntFunction<Product> function = product -> product.getSoldAtStoreTime().getYear();
+
+            return getPopularProduct(val, products, function);
         }
 
         @Override
         public Optional<ProductDTO> findMostUnpopularProductByDate(int val, List<Product> products) {
-            return products.stream()
-                    .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
-                            product.getArrivedAtStoreTime().getYear() >= val)
-                    .collect(Collectors.groupingBy(Function.identity(), counting()))
-                    .entrySet().stream()
-                    .min(Map.Entry.<Product, Long>comparingByValue()
-                            .thenComparingDouble(entry -> entry.getKey().getPrice()))
-                    .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+            ToIntFunction<Product> function = product -> product.getArrivedAtStoreTime().getYear();
+
+            return getUnpopularProduct(val, products, function);
         }
     }, DAY {
         @Override
         public Optional<ProductDTO> findMostPopularProductByDate(int val, List<Product> products) {
-            return products.stream()
-                    .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
-                            product.getSoldAtStoreTime().getDayOfMonth() == val)
-                    .collect(Collectors.groupingBy(Function.identity(), counting()))
-                    .entrySet().stream()
-                    .max(Map.Entry.<Product, Long>comparingByValue()
-                            .thenComparingDouble(product -> product.getKey().getPrice()))
-                    .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+            ToIntFunction<Product> function = product -> product.getSoldAtStoreTime().getDayOfMonth();
+
+            return getPopularProduct(val, products, function);
         }
 
         @Override
         public Optional<ProductDTO> findMostUnpopularProductByDate(int val, List<Product> products) {
-            return products.stream()
-                    .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
-                            product.getSoldAtStoreTime().getDayOfMonth() >= val)
-                    .collect(Collectors.groupingBy(Function.identity(), counting()))
-                    .entrySet().stream()
-                    .min(Map.Entry.<Product, Long>comparingByValue()
-                            .thenComparingDouble(product -> product.getKey().getPrice()))
-                    .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+            ToIntFunction<Product> function = product -> product.getSoldAtStoreTime().getDayOfMonth();
+
+            return getUnpopularProduct(val, products, function);
         }
     };
 
+    protected static Optional<ProductDTO> getUnpopularProduct(int val, List<Product> products, ToIntFunction<Product> function) {
+        return products.stream()
+                .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
+                        function.applyAsInt(product) >= val)
+                .collect(Collectors.groupingBy(Function.identity(), counting()))
+                .entrySet().stream()
+                .min(Map.Entry.<Product, Long>comparingByValue()
+                        .thenComparingDouble(entry -> entry.getKey().getPrice()))
+                .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+    }
+
+    protected static Optional<ProductDTO> getPopularProduct(int val, List<Product> products, ToIntFunction<Product> function) {
+        return products.stream()
+                .filter(product -> product.getProductState().equals(ProductState.SOLD) &&
+                        function.applyAsInt(product) == val)
+                .collect(Collectors.groupingBy(Function.identity(), counting()))
+                .entrySet().stream()
+                .max(Map.Entry.<Product, Long>comparingByValue()
+                        .thenComparingDouble(entry -> entry.getKey().getPrice()))
+                .map(entry -> ProductMapperDTO.mapToProductDTO(entry.getKey()));
+    }
+
     public abstract Optional<ProductDTO> findMostPopularProductByDate(int val, List<Product> products);
+
     public abstract Optional<ProductDTO> findMostUnpopularProductByDate(int val, List<Product> products);
 }
